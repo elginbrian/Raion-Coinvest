11f879e09d6fe287aeccc0e79c0043568c2a4d11
aditya wirz <adityawir0410@gmail.com>
2024-02-29 19:48:34 +0700

    ini file coba baru

---
+++ app/src/main/java/com/raion/coinvest/presentation/login1/loginawal.kt
@@ -0,0 +1,15 @@
+package com.raion.coinvest.presentation.login1
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+@Preview
+fun LoginHome() {
+    Box(modifier = Modifier.fillMaxSize()){
+        BoxSatu()
+    }
+}
